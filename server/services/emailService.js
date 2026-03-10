609ae205559bfee9f41ccb269ee897275edd5370
Daniel <temmylayo817@gmail.com>
2026-03-10 22:59:05 +0100

    Fix device management imports - export sendEmail and fix UAParser import

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -20,7 +20,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
 /**
  * Send an email via Resend
  */
-async function sendEmail({ to, subject, html, text }) {
+export async function sendEmail({ to, subject, html, text }) {
   try {
     const { data, error } = await resend.emails.send({
       from: FROM_EMAIL,
